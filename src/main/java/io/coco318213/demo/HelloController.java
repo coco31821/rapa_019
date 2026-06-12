69e47ce04c4bdd2056e2462dc43a4df1e7bfaac0
coco31821 <coco318213@gmail.com>
2026-06-12 10:00:57 +0900

    feat - 기능 개발

--- src/main/java/io/coco318213/demo/HelloController.java
+++ src/main/java/io/coco318213/demo/HelloController.java
@@ -10,4 +10,8 @@ public class HelloController {
     public String sayHello(){
         return "Hello World";
     }
+    @GetMapping("/bye")
+    public String sayBye(){
+        return "GoodBye";
+    }
 }
